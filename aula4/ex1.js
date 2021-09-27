35815d976edd28f9e03dd93eb547e2ca873ec393
ChoyaSpadin <choya.spadin@gmail.com>
2021-09-27 20:12:42 -0300

    aula4

---
+++ aula4/ex1.js
@@ -0,0 +1,31 @@
+/*DESAFIO 01
+Reajuste salarial
+As empresas @.com resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
+Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+salários até R$ 280,00 (incluindo) : aumento de 20%
+salários entre R\$ 280,00 e R$ 700,00 : aumento de 15%
+salários entre R\$ 700,00 e R$ 1500,00 : aumento de 10%
+salários de R$ 1500,00 em diante : aumento de 5%
+Após o aumento ser realizado, informe na tela:
+o salário antes do reajuste;
+o percentual de aumento aplicado;
+o valor do aumento;
+o novo salário, após o aumento."
+ */
+
+const prompt = require("prompt-sync")();
+
+let valorSalario = +prompt("Digite o valor do salário ");
+
+if (valorSalario <= 280) {
+    percentual = 20;
+} else if (valorSalario <= 700) {
+    percentual = 15;
+} else if (valorSalario <= 1500) {
+    percentual = 10;
+} else {
+    percentual = 5;
+}
+
+console.log("Seu salário é de", valorSalario);
+console.log("Com o reajuste fica",valorSalario + (valorSalario * percentual) / 100);
---
+++ aula4/ex2.js
@@ -0,0 +1,24 @@
+/*DESAFIO 02
+Jogo da adivinhação
+Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 10 e peça para o usuário tentar descobrir qual foi o número escolhido
+ pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+*/
+
+
+const prompt = require("prompt-sync")();
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let number = +prompt('Adivinhe um numero de  1 a 10: ');
+
+while (number !== random) {
+    number = +prompt('Guess a number from 1 to 10: ');
+}
+
+if (number === random) {
+    console.log('You guessed the correct number.');
+} else if (number > random) {
+    console.log("Mais baixo");
+} else if (number < random) {
+    console.log("Mais alto");
+}
